3de496df4e8bc2dc578f2b9fefbd5a4ebe7b8f13
fysoul17 <terry@gixgames.com>
2020-05-16 11:55:45 +1000

    Simplify if-else statement

--- example/lib/Components/chat_view.dart
+++ example/lib/Components/chat_view.dart
@@ -67,93 +67,89 @@ class _ChatViewState extends State<ChatView> {
     return ChatMessages(
       groupId: widget.chatGroupData.id,
       builder: (context, messages, child) {
-        if (messages == null) {
+        if (messages == null || messages.length < 1) {
           return Container();
         } else {
-          if (messages.length < 1) {
-            return Container();
-          } else {
-            List<ChatMessage> chats = messages.reversed.toList();
+          List<ChatMessage> chats = messages.reversed.toList();
 
-            Widget listView = ListView(
-              reverse: true,
-              shrinkWrap: true,
-              controller: _scrollController,
-              padding: EdgeInsets.all(8.0),
-              children: [
-                // Bubble(
-                //   alignment: Alignment.center,
-                //   color: Color.fromARGB(255, 212, 234, 244),
-                //   elevation: 2,
-                //   margin: BubbleEdges.only(top: 8.0),
-                //   child: Text('TODAY', style: TextStyle(fontSize: 13)),
-                // ),
-                if (chats.length > 0)
-                  ...List.generate(chats.length, (index) {
-                    bool isMyMsg = chats[index].senderId == widget.mySenderId;
+          Widget listView = ListView(
+            reverse: true,
+            shrinkWrap: true,
+            controller: _scrollController,
+            padding: EdgeInsets.all(8.0),
+            children: [
+              // Bubble(
+              //   alignment: Alignment.center,
+              //   color: Color.fromARGB(255, 212, 234, 244),
+              //   elevation: 2,
+              //   margin: BubbleEdges.only(top: 8.0),
+              //   child: Text('TODAY', style: TextStyle(fontSize: 13)),
+              // ),
+              if (chats.length > 0)
+                ...List.generate(chats.length, (index) {
+                  bool isMyMsg = chats[index].senderId == widget.mySenderId;
 
-                    Widget infoWidget;
-                    if (chats[index].sendAt == null) {
-                      infoWidget = Container(
-                        margin: const EdgeInsets.only(bottom: 10),
-                        child: SizedBox(
-                            width: 10,
-                            height: 10,
-                            child: CircularProgressIndicator(
-                              strokeWidth: 2,
-                            )),
-                      );
-                    } else {
-                      String dateString = DateFormat("jm").format(chats[index].sendAt.toDate());
-                      int readCount = widget.chatGroupData.participantsData.length - chats[index].readParticipants.length;
-                      infoWidget = Column(
-                        crossAxisAlignment: isMyMsg ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-                        children: <Widget>[
-                          if (readCount > 0)
-                            Text(
-                              "$readCount",
-                              style: defaultTextStyle.copyWith(fontSize: 13, color: Colors.blueAccent),
-                            ),
-                          Text(
-                            dateString,
-                            style: defaultTextStyle.copyWith(fontSize: 11),
-                          ),
-                        ],
-                      );
-                    }
-
-                    Widget bubble = Bubble(
-                      style: isMyMsg ? styleMe : styleSomebody,
-                      child: ConstrainedBox(
-                        constraints: BoxConstraints(maxWidth: widget.chatBubbleMaxWidth),
-                        child: Text(
-                          chats[index].message,
-                          style: defaultTextStyle.copyWith(fontSize: 16),
-                        ),
-                      ),
+                  Widget infoWidget;
+                  if (chats[index].sendAt == null) {
+                    infoWidget = Container(
+                      margin: const EdgeInsets.only(bottom: 10),
+                      child: SizedBox(
+                          width: 10,
+                          height: 10,
+                          child: CircularProgressIndicator(
+                            strokeWidth: 2,
+                          )),
                     );
-
-                    return Row(
-                      mainAxisAlignment: isMyMsg ? MainAxisAlignment.end : MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.end,
+                  } else {
+                    String dateString = DateFormat("jm").format(chats[index].sendAt.toDate());
+                    int readCount = widget.chatGroupData.participantsData.length - chats[index].readParticipants.length;
+                    infoWidget = Column(
+                      crossAxisAlignment: isMyMsg ? CrossAxisAlignment.end : CrossAxisAlignment.start,
                       children: <Widget>[
-                        if (isMyMsg) infoWidget,
-                        bubble,
-                        if (!isMyMsg) infoWidget,
+                        if (readCount > 0)
+                          Text(
+                            "$readCount",
+                            style: defaultTextStyle.copyWith(fontSize: 13, color: Colors.blueAccent),
+                          ),
+                        Text(
+                          dateString,
+                          style: defaultTextStyle.copyWith(fontSize: 11),
+                        ),
                       ],
                     );
-                  }),
-              ],
-            );
+                  }
 
-            // 현재 위치가 가장 하단이 아닐 경우에 스크롤 위치 유지를 위함. (+ 메시지가 추가 됐을 경우만)
-            if (_scrollController.hasClients && _scrollController.offset != 0 && _prevListCount != chats.length) {
-              _scrollController.jumpTo(_scrollController.offset + 45);
-              _prevListCount = chats.length;
-            }
+                  Widget bubble = Bubble(
+                    style: isMyMsg ? styleMe : styleSomebody,
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints(maxWidth: widget.chatBubbleMaxWidth),
+                      child: Text(
+                        chats[index].message,
+                        style: defaultTextStyle.copyWith(fontSize: 16),
+                      ),
+                    ),
+                  );
 
-            return Align(alignment: Alignment.topCenter, child: listView);
+                  return Row(
+                    mainAxisAlignment: isMyMsg ? MainAxisAlignment.end : MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      if (isMyMsg) infoWidget,
+                      bubble,
+                      if (!isMyMsg) infoWidget,
+                    ],
+                  );
+                }),
+            ],
+          );
+
+          // 현재 위치가 가장 하단이 아닐 경우에 스크롤 위치 유지를 위함. (+ 메시지가 추가 됐을 경우만)
+          if (_scrollController.hasClients && _scrollController.offset != 0 && _prevListCount != chats.length) {
+            _scrollController.jumpTo(_scrollController.offset + 45);
+            _prevListCount = chats.length;
           }
+
+          return Align(alignment: Alignment.topCenter, child: listView);
         }
       },
     );
